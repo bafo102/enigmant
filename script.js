bb67d8f4017ea451bba66e08e8469c52c7f68072
bafo102 <huynhthongdat102@gmail.com>
2025-02-16 04:42:05 +0700

    Make rotor droppable and make labels update

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-const element = document.querySelector('.plate');
-// Get the element's height in pixels
-const elementHeight = element.offsetHeight;
-// Get the viewport height in pixels
-const viewportHeight = window.innerHeight;
-// Calculate the height in vh
-const heightInVh = (elementHeight / viewportHeight) * 100;
-console.log('Element height in vh:', heightInVh);
-// const distanceZ = (heightInVh/2)/
-
-
+let rotorTray = ['0', '1', '2', '3', '4', '5'];
+let rotorInUse = ['0', '-', '-', '-']
+let rotorLabels = ['0', 'I', 'II', 'III', 'IV', 'V'];
+const label_1 = document.querySelector("#label-1");
+const label_2 = document.querySelector("#label-2");
+const label_3 = document.querySelector("#label-3");
+const label_4 = document.querySelector("#label-4");
+const label_5 = document.querySelector("#label-5");
+const label_6 = document.querySelector("#label-6");
+const label_7 = document.querySelector("#label-7");
+const label_8 = document.querySelector("#label-8");
 const keys = document.querySelectorAll(".key");
 let targetKey = '';
 
@@ -54,3 +54,79 @@ window.addEventListener('wheel', (event) => {
     event.preventDefault(); // Prevent default scrolling
     console.log(event.deltaY);
 });
+
+$( function() {
+    $( ".rotor" ).draggable({
+        zIndex: 100,
+        // revert: "invalid"
+        revert: function(dropped) {
+            return !dropped; // Revert if not dropped
+        },
+        revertDuration: 100, // Duration in milliseconds
+
+        // function to make empty holder droppable again
+        stop: updateHolderStatus
+    });
+
+    $( ".rotor-holder" ).droppable({
+        drop: function( event, ui ) {
+            // disable busy holder
+            if ($(this).children.length > 0) {
+                $(this).droppable("disable");
+            }
+            // actually move the rotor inside the rotor-holder
+            $( this ).append(ui.draggable);
+            // position the rotor to fit the rotor-holder
+            ui.draggable.position({
+                my: "left top",
+                at: "left top",
+                of: $(this)
+            }),
+            updateRotorLabel();
+        }
+    });
+} );
+
+function updateHolderStatus() {
+    document.querySelectorAll(".rotor-holder").forEach(holder => {
+        if (holder.children.length == 0) {
+            $( `#${holder.id}` ).droppable("enable");
+        }
+    });
+}
+
+function updateRotorLabel() {
+    // reset orders
+    rotorTray = ['0'];
+    rotorInUse = ['0'];
+    // check rotor and update order
+    document.querySelectorAll("#rotor-tray .rotor-holder").forEach(holder => {
+        if (holder.children.length == 0) {
+            rotorTray.push("-");
+        } else {
+            rotorTray.push(holder.firstElementChild.id[holder.firstElementChild.id.length - 1]);
+        }
+    });
+
+    document.querySelectorAll("#rotor-in-use .rotor-holder").forEach(holder => {
+        if (holder.children.length == 0) {
+            rotorInUse.push("-");
+        } else {
+            rotorInUse.push(holder.firstElementChild.id[holder.firstElementChild.id.length - 1]);
+        }
+    });
+
+    console.log('rotorTray is: ', rotorTray);
+    console.log('rotorInUse is: ', rotorInUse);
+
+    // update labels
+    label_1.textContent = rotorTray[1] == '-' ? '-' : rotorLabels[Number(rotorTray[1])];
+    label_2.textContent = rotorTray[2] == '-' ? '-' : rotorLabels[Number(rotorTray[2])];
+    label_3.textContent = rotorTray[3] == '-' ? '-' : rotorLabels[Number(rotorTray[3])];
+    label_4.textContent = rotorTray[4] == '-' ? '-' : rotorLabels[Number(rotorTray[4])];
+    label_5.textContent = rotorTray[5] == '-' ? '-' : rotorLabels[Number(rotorTray[5])];
+
+    label_6.textContent = rotorInUse[1] == '-' ? '-' : rotorLabels[Number(rotorInUse[1])];
+    label_7.textContent = rotorInUse[2] == '-' ? '-' : rotorLabels[Number(rotorInUse[2])];
+    label_8.textContent = rotorInUse[3] == '-' ? '-' : rotorLabels[Number(rotorInUse[3])];
+}
